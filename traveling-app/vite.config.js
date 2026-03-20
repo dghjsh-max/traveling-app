727bcfc3e19d5e5ef3cc02331794544e5d537944
seunghyeon kim <dghjsh@gmail.com>
2026-03-20 11:15:50 +0900

    fix: add base path for github pages

--- traveling-app/vite.config.js
+++ traveling-app/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // GitHub Pages 리포지토리 이름을 base 경로로 설정해야 합니다.
+  base: '/traveling-app/', 
 })
-base: '/traveling-app/'
